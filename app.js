b8dbbc2c9cad119cf4f8e6f87f8d7977edc88c54
congteng <congteng45@gmail.com>
2012-12-03 21:14:56 +0800

    add socket.io and initialized

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
@@ -33,6 +28,8 @@ app.configure('development', function(){
 app.get('/', routes.index);
 // app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+var io = require('socket.io').listen(server);
